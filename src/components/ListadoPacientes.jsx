24d8ed5ec97b71f7b0961c2555654b6677224471
henryposa <luisgasthof@hotmail.com>
2022-04-24 13:43:46 -0500

    si no hay pacientes muestra mensaje

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -3,17 +3,31 @@ import Paciente from "./Paciente"
 const ListadoPacientes = ({ pacientes }) => {
   return (
     <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center ">Listado pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {''}
-        <span className="text-indigo-600 font-bold">Pacientes y citas</span>
-      </p>
-      {pacientes.map((paciente) => (
-        <Paciente
-          key={paciente.id}
-          paciente={paciente}
-        />
-      ))}
+      {/* Si hay pacientes ejecute todo ( sino : imprima no hay pacientes */}
+      {/* Tambien : pacientes.length > 0 */}
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center ">Listado pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {''}
+            <span className="text-indigo-600 font-bold">Pacientes y citas</span>
+          </p>
+          {pacientes.map((paciente) => (
+            <Paciente
+              key={paciente.id}
+              paciente={paciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center ">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando a tus pacientes {''}
+            <span className="text-indigo-600 font-bold">Y apareceran en este lugar</span>
+          </p>
+        </>
+      )}
     </div>
   )
 }
