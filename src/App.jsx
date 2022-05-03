35ef24ec71644f00348f9e5a7b15f3b0490a5cdf
henryposa <luisgasthof@hotmail.com>
2022-05-03 11:29:55 -0500

    creando nuevo header

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
+  // ??: ejecuta la expresion del lado derecho cuando el izquierdo es null o undefined
+  // al abrir o recargar la pagina si no hay nada o eliminan pacientes[] en el localStorage
+  // crea el []
+  // JSON.parse toma el string que está en el Storage y lo transforma en un objeto
   const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+  // la primera vez en la pagina muestra el objeto vacio, al agregar paciente se guarda
+  // en pacientesLS y al recargar el estado inicial es lo que hay en pacientesLS
   const [pacientes, setPacientes] = useState(pacientesLS)
   // Nuevo estado para tomar el objeto con la info inmutable y mandarlo al 
   // formulario para su edicion y es {} porque ahí está el objetoPaciente
   const [paciente, setPaciente] = useState({})
 
   useEffect(() => {
+    // al agregar un paciente, lo manda al Storage
     localStorage.setItem('pacientes', JSON.stringify(pacientes));
   }, [pacientes])
 
